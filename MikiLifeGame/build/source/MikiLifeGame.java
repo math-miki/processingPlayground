878cacf4c3824bdf0383ed10775e2c25e486ccc5
math-miki <math.micky.dtl@gmail.com>
2017-11-02 03:22:15 +0900

    creat basic function of generate new generation of life game

--- MikiLifeGame/build/source/MikiLifeGame.java
+++ MikiLifeGame/build/source/MikiLifeGame.java
@@ -44,7 +44,44 @@ public void drawField() {
 }
 
 public void createNewGeneration() {
-
+  int[][] temp = new int[50][200];
+  for(int y=0; y<50; y++) {
+    for(int x=0; x<200; x++) {
+      int count = 0;
+      if(y == 0 && x == 0) {
+        count += field[y][x+1] + field[y+1][x] + field[y+1][x+1];
+      } else if (y == 0 && x == 49) {
+        count += field[y][x-1] + field[y+1][x] + field[y+1][x-1];
+      } else if (y == 49 && x == 0) {
+        count += field[y][x+1] + field[y-1][x] + field[y-1][x+1];
+      } else if (y == 49 && x == 49) {
+        count += field[y][x-1] + field[y-1][x] + field[y-1][x-1];
+      } else if (y == 0) {
+        count += field[y][x-1] + field[y][x+1] + field[y+1][x] + field[y+1][x-1] + field[y+1][x+1];
+      } else if (y == 49) {
+        count += field[y][x-1] + field[y][x+1] + field[y-1][x] + field[y-1][x-1] + field[y-1][x+1];
+      } else if (x == 0) {
+        count += field[y-1][x] + field[y-1][x+1] + field[y][x+1] + field[y+1][x] + field[y+1][x+1];
+      } else if (x == 49) {
+        count += field[y-1][x] + field[y-1][x-1] + field[y][x-1] + field[y+1][x] + field[y+1][x-1];
+      } else {
+        count += field[y-1][x-1] + field[y-1][x] + field[y-1][x+1] + field[y][x-1] + field[y][x+1] + field[y+1][x-1] + field[y+1][x] + field[y+1][x+1];
+      }
+      int result = 0;
+      if(field[y][x] == 0 && count == 3) {
+        temp[y][x] = 1;
+      } else if(field[y][x] == 1 && count == 2 || count == 2) {
+        temp[y][x] = 1;
+      } else {
+        temp[y][x] = 0;
+      }
+    }
+  }
+  for(int y=0; y<50; y++) {
+    for(int x=0; x<200; x++) {
+      field[y][x] = temp[y][x];
+    }
+  }
 }
   public void settings() {  size(800,200); }
   static public void main(String[] passedArgs) {
